019f88ea828afb458960aaa760958140e2a6501e
iLoveCYaRon <1023460580@qq.com>
2020-06-17 11:07:45 +0800

    增加验证手机号和获取空座位的接口

---
+++ app/src/main/java/moe/sui/dbcontrol/SeatController.java
@@ -0,0 +1,45 @@
+package moe.sui.dbcontrol;
+
+import com.raizlabs.android.dbflow.sql.language.SQLite;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import moe.sui.ds.MealRecord;
+import moe.sui.ds.MealRecord_Table;
+import moe.sui.ds.Seat;
+import moe.sui.ds.Seat_Table;
+
+
+public class SeatController {
+    // 获取所有空座位
+    public static List<Seat> getAvailableSeat() {
+        List<MealRecord> recordList = SQLite.select()
+                                    .from(MealRecord.class)
+                                    .where(MealRecord_Table.endTime.eq(new Date(-1)))
+                                    .queryList();
+
+        // 所有座位列表
+        List<Seat> seatList = SQLite.select()
+                .from(Seat.class)
+                .queryList();
+        List<Long> seatIdList = new ArrayList<>();
+        for (Seat seat:seatList) {
+            seatIdList.add(seat.getSeatId());
+        }
+
+        // 对于每一条忙记录，都从所有座位列表中移除其座位
+        List<Seat> busySeat = new ArrayList<>();
+        for (MealRecord record : recordList) {
+            long busySeatId = record.getRecSeatId().getSeatId();
+            if(seatIdList.indexOf(busySeatId) != -1) {
+                seatList.remove(seatIdList.indexOf(busySeatId));
+            }
+        }
+
+
+        return seatList;
+    }
+
+}
