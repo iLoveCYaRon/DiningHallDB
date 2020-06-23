989376709a2a42abcb0a4ab23fab4158a2fb411f
Dewickol <DSRDewickol@users.noreply.github.com>
2020-06-23 18:30:04 +0800

    写了DBconnect

--- app/src/main/java/moe/sui/dbcontrol/DBconnect.java
+++ app/src/main/java/moe/sui/dbcontrol/DBconnect.java
@@ -1,57 +1,36 @@
 package moe.sui.dbcontrol;
 
+import android.widget.EditText;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import moe.sui.R;
+
 
 public class DBconnect {
-    private static final String driver = "com.mysql.jdbc.Driver";
-    private static final String url = "jdbc:mysql://192.168.56.133:3306/test1?useSSL=false&serverTimezone=GMT";
-    //private static final String url = "jdbc:mysql://192.168.56.133:3306/test1";
-    private static final String user = "testcus";
-    private static final String pwd = "88888888";
-    public static Connection conn;
-
-    public static void linkMysql() {
-//        Connection conn=null;
-        PreparedStatement stmt=null;
-        try {
-            Class.forName(driver).newInstance();
-            System.out.println("驱动加载成功！！！！！");
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        try{
-            conn = DriverManager.getConnection(url,user,pwd);
-            System.out.println("连接数据库成功！！！！！！");
-            String sql = "select * from user";//查询语句
-            stmt= conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()){
-                String id=rs.getString("id");
-                String name=rs.getString("name");
-                String phone=rs.getString("phone");
-                System.out.println(id+"\t"+name+"\t"+phone);
-            }
 
 
+    private static String driver= "com.mysql.jdbc.Driver";
+    private static String url=null;
+    private static String user=null;
+    private static String pwd=null;
+    private static final String databaseName ="dininghall";
+    private static final String SSLTZ ="?useSSL=false&serverTimezone=GMT";
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            if(conn!=null){
-                try {
-                    conn.close();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        }
+    public static void setDBLink(String ip_port,String db_login_usrname,String db_login_pwd) {
+        url ="jdbc:mysql://"+ip_port+databaseName+SSLTZ;
+        user = db_login_usrname;
+        pwd =db_login_pwd;
+
     }
+
+    public static Connection getConnection() throws Exception {
+        Class.forName(driver);
+        return DriverManager.getConnection(url, user, pwd);
+    }
+
 }
 
---
+++ app/src/main/java/moe/sui/dbcontrol/TestConnect.java
@@ -0,0 +1,4 @@
+package moe.sui.dbcontrol;
+
+public class TestConnect {
+}
