5b50ebcdfefbfea51551e7df00e682febdd4ebb5
iLoveCYaRon <1023460580@qq.com>
2020-06-25 17:14:51 +0800

    优化添加就餐记录界面

--- app/src/main/java/moe/sui/RecordActivity.java
+++ app/src/main/java/moe/sui/RecordActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.qmuiteam.qmui.widget.QMUITopBar;
+
 public class RecordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+        initUI();
+    }
+
+    private void initUI() {
+        QMUITopBar topBar = findViewById(R.id.topbar_record);
+        topBar.setTitle("填写就餐信息");
+        topBar.addLeftBackImageButton().setOnClickListener(v -> onBackPressed());
     }
 }
