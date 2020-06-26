569288c129c5d949fae5fffddb51f039bca9b20b
Dewickol <DSRDewickol@users.noreply.github.com>
2020-06-26 17:05:14 +0800

    新增的数据类型
    DineTraffic储存用餐人流
    LineTraffic储存排队人流

---
+++ app/src/main/java/moe/sui/datastruct/DineTraffic.java
@@ -0,0 +1,16 @@
+package moe.sui.datastruct;
+
+public class DineTraffic {
+    public int increaseNum;
+    public int currentNum;
+    public String posName;
+    public String updateTime;
+
+    public DineTraffic(int iNum, int cNum, String pName, String uTime){
+        increaseNum =iNum;
+        currentNum=cNum;
+        posName = pName;
+        updateTime=uTime;
+    }
+
+}
---
+++ app/src/main/java/moe/sui/datastruct/LineTraffic.java
@@ -0,0 +1,13 @@
+package moe.sui.datastruct;
+
+public class LineTraffic {
+    public int currentNum;
+    public String winName;
+    public String updateTime;
+
+    public LineTraffic(int cNum, String wName, String uTime){
+        currentNum=cNum;
+        winName = winName;
+        updateTime=uTime;
+    }
+}
