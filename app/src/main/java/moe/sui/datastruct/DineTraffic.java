a53379a937ca662fe82afd3fe3f7882bc4b18b04
Dewickol <DSRDewickol@users.noreply.github.com>
2020-06-26 17:41:27 +0800

    修改了自定义数据结构
    内含增加了posId

--- app/src/main/java/moe/sui/datastruct/DineTraffic.java
+++ app/src/main/java/moe/sui/datastruct/DineTraffic.java
@@ -3,14 +3,16 @@ package moe.sui.datastruct;
 public class DineTraffic {
     public int increaseNum;
     public int currentNum;
+    public int posId;
     public String posName;
     public String updateTime;
 
-    public DineTraffic(int iNum, int cNum, String pName, String uTime){
+    public DineTraffic(int iNum, int cNum, String pName, String uTime,int pId){
         increaseNum =iNum;
         currentNum=cNum;
         posName = pName;
         updateTime=uTime;
+        posId =pId;
     }
 
 }
